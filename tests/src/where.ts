012509122bfaa3b088898fcf1ae052b876b73b38
Eric Harris-Braun <eric@harris-braun.com>
2021-07-05 08:48:41 -0400

    add bobbo back in

--- tests/src/where.ts
+++ tests/src/where.ts
@@ -13,9 +13,9 @@ module.exports = async (orchestrator) => {
 
     // install your happs into the conductors and destructuring the returned happ data using the same
     // array structure as you created in your installation array.
-    let [alice_where_happ/*, bobbo_where_happ*/] = await installAgents(a_and_b_conductor,  ["alice"/*, 'bobbo'*/])
+    let [alice_where_happ, bobbo_where_happ] = await installAgents(a_and_b_conductor,  ["alice", 'bobbo'])
     const [alice_where] = alice_where_happ.cells
-//    const [bobbo_where] = bobbo_where_happ.cells
+    const [bobbo_where] = bobbo_where_happ.cells
 
 
     // Create a space
