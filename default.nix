0b26c0ae7d8568ac6f68fddb1169418f9099a774
Eric Harris-Braun <eric@harris-braun.com>
2021-07-09 12:03:34 -0400

    update to dip of develop

--- default.nix
+++ default.nix
@@ -8,8 +8,8 @@ let
     holochainVersionId = "custom";
 
     holochainVersion = {
-     rev = "5ec47d73654fb9016da30e3dae0c6fbc63e95f14";
-     sha256 = "05wlp8wdn5bfxbfyxr5hhc9fsi4hlcgj6h8qdilfsp6awh4dyfw2";
+     rev = "8a00e59a4c30c7ffae24f936e193e7e59a893a6b";
+     sha256 = "06qkjsaw7sq01wkfjwhkgfx4dww7qzxn34h710iy6lf5rhkzv05a";
      cargoSha256 = "1i9dpqydkm9fjl63gx5vrlsmg1d9qdy3ci2brdyldsqvibz3lydh";
      bins = {
        holochain = "holochain";
