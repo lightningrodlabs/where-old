d9a40e3ec679f3e2da0324bd7dda5f5f77578141
Eric Harris-Braun <eric@harris-braun.com>
2021-07-12 11:57:40 -0400

    more fun big earth map

--- ui/src/stores.ts
+++ ui/src/stores.ts
@@ -3,15 +3,15 @@ import { writable/*, readable, derived*/ } from 'svelte/store'
 export const spaces = writable(
   [{name: "Earth",
     meta: {
-      url: "https://www.freeworldmaps.net/world/worldmap-physical-hd.jpg",
+      url: "https://h5pstudio.ecampusontario.ca/sites/default/files/h5p/content/9451/images/image-5f6645b4ef14e.jpg",
       size: {x: 1920, y: 1019}
     },
     wheres: [
-      { entry: {location: {x: 540, y: 410}, meta: "My house"},
+      { entry: {location: {x: 1150, y: 450}, meta: "My house"},
         authorPic: "https://i.imgur.com/oIrcAO8.jpg",
         authorName: "Eggy",
         authorPubkey: "mememememememememememememememeeeeee"},
-      { entry: {location: {x: 900, y: 420}, meta: "My apartment"},
+      { entry: {location: {x: 1890, y: 500}, meta: "My apartment"},
         authorPic: "https://i.imgur.com/4BKqQY1.png",
         authorName: "Monk",
         authorPubkey: "sntahoeuabcorchaotbkantgcdoesucd"}
